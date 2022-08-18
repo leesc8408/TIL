e64515ce2af0bd916f7f03a1e842937f46e9563a
leesc8408 <leesc8408@gmail.com>
2022-08-18 11:10:52 +0900

    0818_TIL_전체수정

--- 0727/40_셀프 넘버.py
+++ 0727/40_셀프 넘버.py
@@ -1,3 +1,13 @@
 # https://www.acmicpc.net/problem/4673
-
-for i in range(1,10001):
+d_list = []
+all_list = []
+for n in range(1,10001):
+    all_list.append(n)
+    n_str = str(n)
+    d = n
+    for i in range(len(n_str)):
+        d += int(n_str[i])
+    d_list.append(d)    
+# print(all_list)
+out = set(all_list) - set(d_list)
+print(*sorted(out), sep='\n')
